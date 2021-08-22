70dd7af46e339794436dbf9850cb69bd2c3cbb11
phangs <wlpineda@gmail.com>
2021-08-22 23:49:33 +0800

    initial release

---
+++ public/app.js
@@ -0,0 +1,46 @@
+const vattrib = "?rel=0&amp;autoplay=1&mute=1&vq=small&showinfo=0&controls=1"
+
+function loadSlot1() {
+    let embed1 = $('#tb1').val().replace('https://youtu.be/', 'https://www.youtube.com/embed/')
+    $('#iframe1').attr('src', embed1+vattrib).fadeIn('fast')
+}
+
+function loadSlot2() {
+    let embed2 = $('#tb2').val().replace('https://youtu.be/', 'https://www.youtube.com/embed/')
+    $('#iframe2').attr('src', embed2+vattrib).fadeIn('fast')
+}
+
+function loadSlot3() {
+    let embed3 = $('#tb3').val().replace('https://youtu.be/', 'https://www.youtube.com/embed/')
+    $('#iframe3').attr('src', embed3+vattrib).fadeIn('fast')
+}
+
+function loadSlot4() {
+    let embed4 = $('#tb4').val().replace('https://youtu.be/', 'https://www.youtube.com/embed/')
+    $('#iframe4').attr('src', embed4+vattrib).fadeIn('fast')
+}
+
+function loadSlot5() {
+    let embed5 = $('#tb5').val().replace('https://youtu.be/', 'https://www.youtube.com/embed/')
+    $('#iframe5').attr('src', embed5+vattrib).fadeIn('fast')
+}
+
+function loadSlot6() {
+    let embed6 = $('#tb6').val().replace('https://youtu.be/', 'https://www.youtube.com/embed/')
+    $('#iframe6').attr('src', embed6+vattrib).fadeIn('fast')
+}
+
+function loadSlot7() {
+    let embed7 = $('#tb7').val().replace('https://youtu.be/', 'https://www.youtube.com/embed/')
+    $('#iframe7').attr('src', embed7+vattrib).fadeIn('fast')
+}
+
+function loadSlot8() {
+    let embed8 = $('#tb8').val().replace('https://youtu.be/', 'https://www.youtube.com/embed/')
+    $('#iframe8').attr('src', embed8+vattrib).fadeIn('fast')
+}
+
+function loadSlot9() {
+    let embed9 = $('#tb9').val().replace('https://youtu.be/', 'https://www.youtube.com/embed/')
+    $('#iframe9').attr('src', embed9+vattrib).fadeIn('fast')
+}
